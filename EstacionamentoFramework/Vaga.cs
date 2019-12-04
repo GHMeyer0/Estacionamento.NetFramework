7f70c9e0b4405d2406acfe9428e571dce799a965
Azure DevOps <azuredevops@microsoft.com>
2019-12-03 21:05:12 -0300

    Removido metodos não utilizados

--- EstacionamentoFramework/Vaga.cs
+++ EstacionamentoFramework/Vaga.cs
@@ -23,36 +23,6 @@ namespace EstacionamentoFramework
             }
             return vagas;
         }
-
-        public static int BuscarVagaLivre(Vaga[] vagas)
-        {
-            foreach (var vaga in vagas)
-            {
-                if (vaga.Livre)
-                {
-                    return vaga.Numero;
-                }
-            }
-            return 0;
-        }
-
-        public static int BuscarVagaOcupada(Vaga[] vagas)
-        {
-            var random = new Random();
-            List<int> vagas_ocupadas = new List<int>();
-            for (int i = 0; i < vagas.Length; i++)
-            {
-                if (!vagas[i].Livre)
-                {
-                    int x = vagas[i].Numero;
-                    vagas_ocupadas.Add(x);
-                }
-            }
-            
-            int index = random.Next(vagas_ocupadas.Count);
-            return vagas_ocupadas[index];
-        }
-
         public static void OcupaVaga(int numeroVaga)
         {
             FormEstacionamento.estacionamento.vagas[numeroVaga].Livre = false;
